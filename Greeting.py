f04990af5503a094ed49975a735d45adbf823910
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-01 20:30:56 +0300

    Greeting message

--- Greeting.py
+++ Greeting.py
@@ -1,1 +1,8 @@
 # return a greeting message with name and age as key word arguments
+
+def greeting(name, age, message = "Hello"):
+    return f"{message} {name} you are {age}"
+
+message1 = greeting(name = "Frank", age=22)
+
+print(message1)
