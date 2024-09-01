4ac57581ae36d4210f4adf24881febca3d7f75cf
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-01 20:25:13 +0300

    Volume of a cube with keyword arguments

---
+++ Volume.py
@@ -0,0 +1,9 @@
+#  volume of a cube with keyword arguments - (length, width and width)
+
+def volume_cube(length, width, height):
+    volume = length * width * height
+    return volume
+
+result = volume_cube(length = 23, width = 34, height = 34)
+
+print("This is your volume:", result)
