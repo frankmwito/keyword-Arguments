6a1cea4d9a2a42299a47837f506f21804e27e0d7
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-01 20:47:11 +0300

    return keyword arguments as a tuple

--- colors.py
+++ colors.py
@@ -1,1 +1,8 @@
 # take colors as keyword arguments, components of a color and returns the color as a tuple
+
+def color_tuple(color1, color2, color3):
+    return (color1, color2, color3)
+
+color_tuple = color_tuple(color1= "red", color2= "green", color3= "blue")
+
+print(color_tuple)
