cb04eafb0a87917fc554119729df9815d39998c1
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-01 20:40:41 +0300

    area of a circle using modules

--- AreaOfCircle.py
+++ AreaOfCircle.py
@@ -1,1 +1,10 @@
 # return the area of a circle accepting radius as a keyword argument
+import math
+
+def area_circle(radius):
+    area = math.pi * math.pow(radius, 2)
+    return area
+
+result = area_circle(radius=22)
+
+print("The area of circle is:", round(result, 2))
